b212591578f51ecb6823e52c6f832c79ac260dfb
thamseer47 <thamseerahamed@gmail.com>
2025-11-18 11:58:53 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "eastus"
+}
+
+variable "vm_admin" {
+  description = "Admin username for VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
