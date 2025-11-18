331793cd5e9a8900ec3d40506aaf54de2fd01061
thamseer47 <thamseerahamed@gmail.com>
2025-11-18 11:58:03 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,1 +1,16 @@
+terraform {
+  required_version = ">= 1.4.0"
 
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+
+  backend "local" {}  # You can switch to Azure storage backend later
+}
+
+provider "azurerm" {
+  features {}
+}
