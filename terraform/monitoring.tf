82bc095e204efc1fde296dcd45a950738328dcd3
thamseer47 <thamseerahamed@gmail.com>
2025-11-27 11:20:11 +0530

    Update monitoring.tf

--- terraform/monitoring.tf
+++ terraform/monitoring.tf
@@ -1,13 +1,29 @@
 ############################################################
-# Log Analytics Workspace
+# Variables required by monitoring.tf
 ############################################################
-resource "azurerm_log_analytics_workspace" "law" {
-  name                = "app-law"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.app.name
-  sku                 = "PerGB2018"
-  retention_in_days   = 30
+
+variable "prefix" {
+  description = "Prefix for naming diagnostic settings"
+  default     = "app"
 }
+
+############################################################
+# Log Analytics Workspace (IMPORTED RESOURCE)
+# COMMENT OUT if workspace already exists
+############################################################
+
+# resource "azurerm_log_analytics_workspace" "law" {
+#   name                = "app-law"
+#   location            = var.location
+#   resource_group_name = azurerm_resource_group.app.name
+#   sku                 = "PerGB2018"
+#   retention_in_days   = 30
+# }
+
+############################################################
+# Diagnostic Settings for Application Gateway
+############################################################
+
 resource "azurerm_monitor_diagnostic_setting" "appgw_diag" {
   name                       = "${var.prefix}-appgw-diag"
   target_resource_id         = azurerm_application_gateway.appgw.id
@@ -15,33 +31,19 @@ resource "azurerm_monitor_diagnostic_setting" "appgw_diag" {
 
   enabled_log {
     category = "ApplicationGatewayAccessLog"
-
-    retention_policy {
-      enabled = false
-    }
   }
 
   enabled_log {
     category = "ApplicationGatewayPerformanceLog"
-
-    retention_policy {
-      enabled = false
-    }
   }
 
   enabled_log {
     category = "ApplicationGatewayFirewallLog"
-
-    retention_policy {
-      enabled = false
-    }
   }
 
   metric {
     category = "AllMetrics"
-
-    retention_policy {
-      enabled = false
-    }
+    enabled  = true
   }
 }
+
