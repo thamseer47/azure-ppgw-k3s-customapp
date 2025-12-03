ce3c487c70e239a26c2eacff43970c94fad4a214
thamseer47 <thamseerahamed@gmail.com>
2025-12-03 12:33:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,65 @@
-variable "location" {
+variable "subscription_id" {
+  description = "Azure Subscription ID (optional when using AZURE_CREDENTIALS in GitHub Actions)"
   type        = string
-  default     = "centralindia"
+  default     = ""
+}
+
+variable "tenant_id" {
+  description = "Azure Tenant ID"
+  type        = string
+  default     = ""
+}
+
+variable "resource_group_name" {
+  description = "Existing resource group name where VM will be created"
+  type        = string
+  default     = "rg-app"
+}
+
+variable "prefix" {
+  description = "Naming prefix for resources (vm, nic, publicip, vnet, subnet, nsg)"
+  type        = string
+  default     = "k3s"
+}
+
+variable "location" {
   description = "Azure region"
-}
-
-variable "vm_admin" {
-  type    = string
-  default = "azureuser"
-}
-
-variable "ssh_key_path" {
-  type    = string
-  default = "~/.ssh/id_rsa.pub"
+  type        = string
+  default     = "Central India"
 }
 
 variable "vm_size" {
-  type    = string
-  default = "Standard_B1s"
+  description = "VM size"
+  type        = string
+  default     = "Standard_B2ms"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key contents (for admin user). Use file() with terraform.tfvars or set TF_VAR_ssh_public_key env var."
+  type        = string
+  default     = ""
+}
+
+variable "vnet_address_space" {
+  description = "VNet address space"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "subnet_prefix" {
+  description = "Subnet prefix"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "allowed_node_ports" {
+  description = "List of NodePort ports to allow in NSG (example values, adjust if needed)"
+  type        = list(number)
+  default     = [32302]
 }
