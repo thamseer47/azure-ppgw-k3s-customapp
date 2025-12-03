668e18a4ca873b76a6d0237dc058f44c27df3b71
thamseer47 <thamseerahamed@gmail.com>
2025-12-03 12:32:38 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,18 @@
 terraform {
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = ">= 3.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-
-  subscription_id = "bff7c565-ed2e-48c9-bd8d-9285d45dca59"
+  # If running in CI using the AZURE_CREDENTIALS (sdk-auth) secret, no further config is required.
+  # If running locally without env auth, you can uncomment and set these:
+  # subscription_id = var.subscription_id
+  # tenant_id       = var.tenant_id
 }
