8433929c9641a1e84b2ab683b1dbaae8dadcd594
thamseer47 <thamseerahamed@gmail.com>
2025-12-04 12:37:17 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "~> 4.54.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-terraform"        # change to your tfstate RG
+    storage_account_name = "tfstate<uniquesuffix>"  # created earlier
+    container_name       = "tfstate"
+    key                  = "azure-ppgw-k3s-customapp.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
